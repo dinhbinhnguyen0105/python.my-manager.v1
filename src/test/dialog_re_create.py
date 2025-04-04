b755b0a95d9816a024292734381ea4f99d5cda6e
DinhBinh Nguyen <dinhbinhnguyen@192.168.1.11.non-exists.ptr.local>
2025-04-04 16:12:38 +0700

    from macmini

--- src/test/dialog_re_create.py
+++ src/test/dialog_re_create.py
@@ -1,10 +1,25 @@
 from PyQt6.QtWidgets import QApplication
 from src.views.dialog_create_real_estate import DialogCreateRealEstate
 from src.models.real_estate_database import initialize_products_database
+from src.models.real_estate_model import RealEstateProductModel
+from src.controllers.real_estate_controller import RealEstateController
+
+
+def handle_accepted(new_product):
+    print(new_product)
+    # model = RealEstateProductModel()
+    # controller = RealEstateController(model)
+    # controller.add_product(new_product)
+
 
 if __name__ == "__main__":
     app = QApplication([])
     initialize_products_database()
+    # model = RealEstateProductModel()
+    # controller = RealEstateController(model)
+    # print(controller.read_all_product())
     dialog = DialogCreateRealEstate()
     dialog.show()
+    dialog.accepted.connect(lambda: handle_accepted(dialog.get_fields()))
+    dialog.rejected.connect(lambda: print("Dialog rejected"))
     app.exec()
--- src/views/dialog_create_real_estate.py
+++ src/views/dialog_create_real_estate.py
@@ -1,16 +1,15 @@
 # src/views/dialog_create_real_estate.py
-from PyQt6.QtCore import Qt, pyqtSignal
+from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QDialog, QDialogButtonBox, QMessageBox
 from PyQt6.QtGui import QDragEnterEvent, QDropEvent, QPixmap
-from PyQt6.QtWidgets import QDialog, QLabel, QDialogButtonBox, QMessageBox
-from src.ui.dialog_create_real_estate_ui import Ui_DialogCreateRealEstate
+from src.ui.dialog_real_estate_ui import Ui_DialogRealEstate
 from src.configs.real_estate_product import RealEstateProductConfigs
 from src.controllers.real_estate_controller import RealEstateController
-from src.models.real_estate_model import RealEstateProductModel
 
 
-class DialogCreateRealEstate(QDialog, Ui_DialogCreateRealEstate):
+class DialogCreateRealEstate(QDialog, Ui_DialogRealEstate):
     def __init__(self, parent=None):
-        super().__init__(parent)  # Initialize QDialog
+        super().__init__(parent)
         self.fields = {
             "image_path": [],
             "option": "",
@@ -30,130 +29,134 @@ class DialogCreateRealEstate(QDialog, Ui_DialogCreateRealEstate):
             "description": "",
             "status": 1,
         }
+
         self.setupUi(self)
-
-        self.model = RealEstateProductModel()
-        self.controller = RealEstateController(self.model)
-
         self.setWindowTitle("Create Real Estate")
-        self.setFixedSize(self.size())
 
-        self.legal.setDisabled(True)
+        self._setupImageDrop()
 
-        self.sell.clicked.connect(
-            lambda: self._setupUi("sell"))
-        self.rent.clicked.connect(
-            lambda: self._setupUi("rent"))
-        self.assignment.clicked.connect(
-            lambda: self._setupUi("assignment"))
+        self.option_assignment_w.clicked.connect(
+            lambda: self.initialize_ui("assignment")
+        )
+        self.option_assignment_w.clicked.connect(
+            lambda: self.set_field("option", "assignment")
+        )
+        self.option_sell_w.clicked.connect(
+            lambda: self.initialize_ui("sell")
+        )
+        self.option_sell_w.clicked.connect(
+            lambda: self.set_field("option", "sell")
+        )
+        self.option_rent_w.clicked.connect(
+            lambda: self.initialize_ui("rent")
+        )
+        self.option_rent_w.clicked.connect(
+            lambda: self.set_field("option", "rent")
+        )
 
-        self.category.currentIndexChanged.connect(lambda: self._setField(
-            "category", self.category.currentData()))
-        self.province.currentIndexChanged.connect(lambda: self._setField(
-            "province", self.province.currentData()))
-        self.district.currentIndexChanged.connect(lambda: self._setField(
-            "district", self.district.currentData()))
-        self.ward.currentIndexChanged.connect(lambda: self._setField(
-            "ward", self.ward.currentData()))
-        self.street.textChanged.connect(lambda: self._setField(
-            "street", self.street.text()))
-        self.area.textChanged.connect(lambda: self._setField(
-            "area", self.area.text()))
-        self.price.textChanged.connect(lambda: self._setField(
-            "price", self.price.text()))
-        self.function.textChanged.connect(lambda: self._setField(
-            "function", self.function.text()))
-        self.structure.textChanged.connect(lambda: self._setField(
-            "structure", self.structure.text()))
-        self.building_line.currentIndexChanged.connect(lambda: self._setField(
-            "building_line", self.building_line.currentData()))
-        self.furniture.currentIndexChanged.connect(lambda: self._setField(
-            "furniture", self.furniture.currentData()))
-        self.legal.currentIndexChanged.connect(lambda: self._setField(
-            "legal", self.legal.currentData()))
-        self.description.textChanged.connect(lambda: self._setField(
-            "description", self.description.toPlainText()))
+        self.pid_input.textChanged.connect(lambda: self.set_field(
+            "pid", self.pid_input.text()))
+        self.categories_input.currentIndexChanged.connect(
+            lambda: self.set_field("category", self.categories_input.currentData()))
+        self.provinces_input.currentIndexChanged.connect(
+            lambda: self.set_field("province", self.provinces_input.currentData()))
+        self.districts_input.currentIndexChanged.connect(
+            lambda: self.set_field("district", self.districts_input.currentData()))
+        self.building_line_s_input.currentIndexChanged.connect(lambda: self.set_field(
+            "building_line", self.building_line_s_input.currentData()))
+        self.furniture_s_input.currentIndexChanged.connect(
+            lambda: self.set_field("furniture", self.furniture_s_input.currentData()))
+        self.legal_s_input.currentIndexChanged.connect(
+            lambda: self.set_field("legal", self.legal_s_input.currentData()))
+        self.street_input.textChanged.connect(
+            lambda: self.set_field("street", self.street_input.text()))
+        self.area_input.textChanged.connect(
+            lambda: self.set_field("area", self.area_input.text()))
+        self.price_input.textChanged.connect(
+            lambda: self.set_field("price", self.price_input.text()))
+        self.function_input.textChanged.connect(
+            lambda: self.set_field("function", self.function_input.text()))
+        self.structure_input.textChanged.connect(
+            lambda: self.set_field("structure", self.structure_input.text()))
+        self.description_input.textChanged.connect(lambda: self.set_field(
+            "description", self.description_input.toPlainText()))
 
+        self.buttonBox.accepted.disconnect()
         self.btn_save = self.buttonBox.button(
             QDialogButtonBox.StandardButton.Save)
-        self.btn_save.disconnect()
         self.btn_save.clicked.connect(self.on_accept)
         self.buttonBox.rejected.connect(self.on_reject)
 
-        self.setupImageDrop()
+    def get_fields(self):
+        return self.fields
 
-    def _setupUi(self, option):
-        self.fields["option"] = option
-        self.setupComboBox()
-        if option == "rent" or option == "assignment":
-            self.legal.setDisabled(True)
-        else:
-            self.legal.setDisabled(False)
-        # Generate a unique PID
+    def load_fields(self): pass
 
-        self.fields["pid"] = self.controller.generate_pid(option)
-        self.pid.setText(self.fields["pid"])
-
-    def _setField(self, field, value):
+    def set_field(self, field, value):
+        if not value:
+            return
         value = value.strip()
         if not field == "description":
             value = value.lower()
         self.fields[field] = value
 
-    def setupComboBox(self):
-        # Setup combo boxes with data from RealEstateProductConfigs
-        configs = RealEstateProductConfigs()
-        # category
-        self.category.clear()
-        for category in configs.categories():
-            self.category.addItem(category.get(
-                "name")[0].capitalize(), category["value"])
-        # province
-        self.province.clear()
-        for province in configs.provinces():
-            self.province.addItem(province.get(
-                "name")[0].capitalize(), province["value"])
-
-        # district
-        self.district.clear()
-        for district in configs.districts():
-            self.district.addItem(district.get(
-                "name")[0].capitalize(), district["value"])
-        # ward
-        self.ward.clear()
-        for ward in configs.wards():
-            self.ward.addItem(ward.get(
-                "name")[0].capitalize(), ward["value"])
-        # building_line
-        self.building_line.clear()
-        for building_line in configs.building_line_s():
-            self.building_line.addItem(building_line.get(
-                "name")[0].capitalize(), building_line["value"])
-        # legal
-        self.legal.clear()
-        for legal in configs.legal_s():
-            self.legal.addItem(legal.get(
-                "name")[0].capitalize(), legal["value"])
-        # furniture
-        self.furniture.clear()
-        for furniture in configs.furniture_s():
-            self.furniture.addItem(furniture.get(
-                "name")[0].capitalize(), furniture["value"])
+    def on_reject(self): pass
 
     def on_accept(self):
-        # Handle the accept button click
-        if self.validate():
-            print("Fields:", self.fields)
-            self.controller.add_product(self.fields)
-            self.accept()
+        if self.validate_fields():
+            super().accept()
         else:
             return False
 
-    def on_reject(self):
-        # Handle the reject button click
-        pass
+    def initialize_ui(self, option):
+        self.pid_input.setText(RealEstateController.generate_pid(option))
 
-    def validate(self):
+        configs = RealEstateProductConfigs()
+        # category
+        self.categories_input.clear()
+        for category in configs.categories():
+            self.categories_input.addItem(category.get(
+                "name")[0].capitalize(), category["value"])
+        # province
+        self.provinces_input.clear()
+        for province in configs.provinces():
+            self.provinces_input.addItem(province.get(
+                "name")[0].capitalize(), province["value"])
+
+        # district
+        self.districts_input.clear()
+        for district in configs.districts():
+            self.districts_input.addItem(district.get(
+                "name")[0].capitalize(), district["value"])
+        # ward
+        self.wards_input.clear()
+        for ward in configs.wards():
+            self.wards_input.addItem(ward.get(
+                "name")[0].capitalize(), ward["value"])
+        # building_line
+        self.building_line_s_input.clear()
+        for building_line in configs.building_line_s():
+            self.building_line_s_input.addItem(building_line.get(
+                "name")[0].capitalize(), building_line["value"])
+        # legal
+        self.legal_s_input.clear()
+        for legal in configs.legal_s():
+            self.legal_s_input.addItem(legal.get(
+                "name")[0].capitalize(), legal["value"])
+        # furniture
+        self.furniture_s_input.clear()
+        for furniture in configs.furniture_s():
+            self.furniture_s_input.addItem(furniture.get(
+                "name")[0].capitalize(), furniture["value"])
+
+        if option == "assignment" or option == "rent":
+            self.legal_s_input.setDisabled(True)
+        else:
+            self.legal_s_input.setDisabled(False)
+        self.provinces_input.setDisabled(True)
+        self.districts_input.setDisabled(True)
+
+    def validate_fields(self):
         if len(self.fields.get("image_path")) < 1:
             QMessageBox.critical(
                 None, "Error", "Please select at least one image.")
@@ -161,13 +164,13 @@ class DialogCreateRealEstate(QDialog, Ui_DialogCreateRealEstate):
         if not self.fields.get("street"):
             QMessageBox.critical(None, "Error", "Street cannot be empty.")
             return False
-        if not self.fields.get("area") or self.str_to_float(self.fields.get("area")) == False:
+        if not self.fields.get("area") or self._str_to_float(self.fields.get("area")) == False:
             QMessageBox.critical(None, "Error", "Area must be a number.")
             return False
-        if not self.fields.get("price") or self.str_to_float(self.fields.get("price")) == False:
+        if not self.fields.get("price") or self._str_to_float(self.fields.get("price")) == False:
             QMessageBox.critical(None, "Error", "Price must be a number.")
             return False
-        if not self.fields.get("structure") or self.str_to_float(self.fields.get("structure")) == False:
+        if not self.fields.get("structure") or self._str_to_float(self.fields.get("structure")) == False:
             QMessageBox.critical(None, "Error", "Structure cannot be empty.")
             return False
         if not self.fields.get("function"):
@@ -178,10 +181,9 @@ class DialogCreateRealEstate(QDialog, Ui_DialogCreateRealEstate):
             QMessageBox.critical(
                 None, "Error", "Description cannot be empty.")
             return False
-
         return True
 
-    def str_to_float(self, str: str):
+    def _str_to_float(self, str: str):
         try:
             _result = float(str)
             if _result == 0:
@@ -190,31 +192,30 @@ class DialogCreateRealEstate(QDialog, Ui_DialogCreateRealEstate):
         except ValueError:
             return False
 
-    def setupImageDrop(self):
-        # Configure self.images as a drag-and-drop widget
-        self.images.setAcceptDrops(True)
+    def _setupImageDrop(self):
+        self.image_input.setAcceptDrops(True)
+        self.image_input.dragEnterEvent = self._imagesDragEnterEvent
+        self.image_input.dropEvent = self._imagesDropEvent
 
-        # Override dragEnterEvent and dropEvent for self.images
-        self.images.dragEnterEvent = self.imagesDragEnterEvent
-        self.images.dropEvent = self.imagesDropEvent
-
-    def imagesDragEnterEvent(self, event: QDragEnterEvent):
+    def _imagesDragEnterEvent(self, event: QDragEnterEvent):
         if event.mimeData().hasUrls():
-            event.acceptProposedAction()
+            event.accept()
+        else:
+            event.ignore()
 
-    def imagesDropEvent(self, event: QDropEvent):
+    def _imagesDropEvent(self, event: QDropEvent):
         if event.mimeData().hasUrls():
-            self.fields["image_path"] = [url.toLocalFile()
-                                         for url in event.mimeData().urls()]
-            self.handleDroppedImages(self.fields["image_path"])
+            images = [url.toLocalFile() for url in event.mimeData().urls()]
+            self._handleDroppedImages(images)
+            self.fields["image_path"] = images
 
-    def handleDroppedImages(self, image_paths):
+    def _handleDroppedImages(self, image_paths):
         if image_paths:
             pixmap = QPixmap(image_paths[0])
             if not pixmap.isNull():
-                self.images.setPixmap(pixmap.scaled(
-                    self.images.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
+                self.image_input.setPixmap(pixmap.scaled(
+                    self.image_input.size(), Qt.AspectRatioMode.KeepAspectRatio, Qt.TransformationMode.SmoothTransformation))
             else:
-                self.images.setText("Failed to load image.")
+                self.image_input.setText("Failed to load image.")
         else:
-            self.images.setText("No images dropped.")
+            self.image_input.setText("No images dropped.")
